543c779d2cbbef9d3d962676c8aa7d4d4fb7d417
chrishemmens <chrishemmens@hotmail.com>
2020-06-26 01:31:36 +0200

    trade fix

--- schema/Route.js
+++ schema/Route.js
@@ -182,28 +182,10 @@ global.trading = false;
 			// this.tradeY.deviate(this.profitFactorY);
 			// this.tradeZ.deviate(this.profitFactorZ);
 
-			var _this = this;
 			this.tradeX.execute();
+			this.tradeY.execute();
 			this.tradeZ.execute();
-	
-			Util.when(
-				function() {
-					return _this.tradeX.requested && !_this.tradeX.responeded;
-				},
-				function() {
-					_this.tradeY.execute();
-					Util.when(
-						function() {
-							return _this.tradeY.requested && !_this.tradeY.responeded;
-						},
-						function() {
-							_this.tradeZ.execute();
-						},
-						100
-					);
-				},
-				100
-			);
+
 
 			Util.when(
 				function() {
