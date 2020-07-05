a7dd3248c3b1175920299357664b7da4997fb0a8
chrishemmens <chrishemmens@hotmail.com>
2020-07-06 00:24:03 +0200

    no message

--- application/model/Arbitrage.js
+++ application/model/Arbitrage.js
@@ -8,6 +8,8 @@ module.exports = class Arbitrage extends Model {
     static OrderBook = require('./OrderBook.js');
     static Balance = require('./Balance.js');
     static Route = require('./Route.js');
+    static Trade = require('./Trade.js');
+    static Order = require('./Order.js');
 
     static async start() {
         console.log('Initializing Arbitrages...');
@@ -16,12 +18,23 @@ module.exports = class Arbitrage extends Model {
         await Arbitrage.Market.init();
         await Arbitrage.OrderBook.init();
         await Arbitrage.Balance.init();
+        await Arbitrage.Trade.init();
+        await Arbitrage.Order.init();
 
         setTimeout(() => {
             console.log('Initializing Routes...');
             Arbitrage.Route.init();
         }, 5000);
 
+    }
+
+    static consoleOutput() {
+        return Arbitrage.Balance.consoleOutput()
+            + Arbitrage.Route.consoleOutput()
+            + Arbitrage.Route.consoleOutput()
+            + Arbitrage.Route.consoleOutput()
+            + Arbitrage.Trade.consoleOutput()
+            + Arbitrage.Order.consoleOutput();
     }
 
     static stop() {
