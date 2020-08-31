e7ed229751a033f50c54b8e9b1532edcfcfde0ef
chrishemmens <chrishemmens@hotmail.com>
2020-09-01 01:58:09 +0200

    no message

--- application/controller/BalanceController.js
+++ application/controller/BalanceController.js
@@ -1,1 +1,12 @@
-//stub
+var Controller = require('../../system/Controller.js');
+
+module.exports = class ArbitrageController extends Controller {
+
+    static Arbitrage = require('../model/Arbitrage.js');
+
+    static async actionIndex(uriParts, request, response) {
+        response.send(await ArbitrageController.Arbitrage.consoleOutput());
+    }
+    
+
+};
