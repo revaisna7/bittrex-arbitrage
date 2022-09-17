c8cddee69cea52a1f44e49cabedebe10b26efbcb
chrishemmens <chrishemmens@hotmail.com>
2022-09-17 22:09:13 +0200

    fixes

--- web/js/currency.js
+++ web/js/currency.js
@@ -1,3 +1,7 @@
 $('#tab ul li[data-for="currencies"]').click(function(e){
    page('currency/config', 'currencies'); 
 });
+$('table tr').click(function(e) {
+    alert(1);
+    $(this).find('input').prop('checked', !$(this).find('input').prop('checked'));
+})
